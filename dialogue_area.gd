75dd5e0a4bbf6403fab042a9ae53c2649cefc18f
cvr8924 <69737122+cvr8924@users.noreply.github.com>
2025-10-07 18:22:39 -0400

    commit :')

---
+++ dialogue_area.gd
@@ -0,0 +1,17 @@
+extends Area2D
+
+@export var dialog_key = ""
+var area_active = false
+
+func _input(event):
+	if area_active and event.is_action_pressed("ui_interact"):
+		DialogueSignalBus.emit_signal("display_dialog",dialog_key)
+		
+
+func _on_area_entered(area: Area2D) -> void:
+	area_active = true
+
+
+
+func _on_area_exited(area: Area2D) -> void:
+	area_active = false
