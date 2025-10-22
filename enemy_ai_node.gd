b2e4f03b68d6de9d1385c8026bea80f1da564d65
cvr8924 <69737122+cvr8924@users.noreply.github.com>
2025-10-22 17:11:48 -0400

    eod
    ~CR

--- enemy_ai_node.gd
+++ enemy_ai_node.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var path_follow : PathFollow2D = $Path2D/PathFollow2D
-@export var speed = 100
+@export var speed = 60
 
 func _process(delta: float) -> void:
 	path_follow.progress += speed*delta
